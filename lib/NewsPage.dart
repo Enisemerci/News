0eb8e9677a5a0671db9b63d1006ea1575fa75408
Enis <enissemerci2@gmail.com>
2024-01-02 16:24:09 +0300

    News
    kategorilere ayrıldı

--- lib/NewsPage.dart
+++ lib/NewsPage.dart
@@ -1,21 +1,24 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:onlynews/colors.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class NewsPage extends StatefulWidget {
+  final String category;
+
+  NewsPage({required this.category});
+
   @override
   _NewsPageState createState() => _NewsPageState();
 }
 
-//science, sport, entertaiment, technology(egitim),business, health
 class _NewsPageState extends State<NewsPage> {
   Future<List<Map<String, dynamic>>> fetchNews() async {
     final apiKey = 'a1987496ddf64d83b80b1824027d7f07';
     final apiUrl =
-        'https://newsapi.org/v2/top-headlines?country=tr&category=science&apiKey=$apiKey';
+        'https://newsapi.org/v2/top-headlines?country=tr&category=${widget.category}&apiKey=$apiKey';
 
     final response = await http.get(Uri.parse(apiUrl));
 
@@ -57,17 +60,14 @@ class _NewsPageState extends State<NewsPage> {
         future: fetchNews(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            //  ekranda "yükleniyor" göstergesi
             return Center(
               child: CircularProgressIndicator(),
             );
           } else if (snapshot.hasError) {
-            // Hata durumunda
             return Center(
               child: Text('Veriler çekilirken hata oluştu: ${snapshot.error}'),
             );
           } else {
-            // Veriler çekildiyse başarılı
             List<Map<String, dynamic>>? newsList = snapshot.data;
 
             return SingleChildScrollView(
@@ -75,7 +75,6 @@ class _NewsPageState extends State<NewsPage> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    // Verileri liste olarak listtile içinde gösterdim
                     for (var news in newsList!)
                       Padding(
                         padding: const EdgeInsets.all(8.0),
--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -8,8 +8,9 @@ import 'package:onlynews/userinput.dart';
 
 class mainPage extends StatelessWidget {
   const mainPage({Key? key}) : super(key: key);
-//cardlar
-  Widget cards(BuildContext context, String cardurl, String title) {
+
+  Widget cards(
+      BuildContext context, String cardurl, String title, String category) {
     return Card(
       color: HexColor(primarycolor),
       child: Column(
@@ -18,17 +19,18 @@ class mainPage extends StatelessWidget {
             title,
             style: TextStyle(color: HexColor(secondcolor)),
           ),
-          //tıklanabilir image alıyorum
           GestureDetector(
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => NewsPage()),
+                MaterialPageRoute(
+                  builder: (context) => NewsPage(category: category),
+                ),
               );
             },
             child: Image.asset(
               cardurl,
-              width: double.infinity, //
+              width: double.infinity,
               height: 150,
               fit: BoxFit.cover,
             ),
@@ -41,104 +43,115 @@ class mainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: HexColor(secondcolor),
-        appBar: AppBar(
-          toolbarHeight: 90,
-          backgroundColor: HexColor(primarycolor),
-          title: Row(
-            children: [
-              SizedBox(
-                width: 0,
-              ),
-              ClipOval(
-                child: SizedBox(
-                  width: 250,
-                  height: 120,
-                  child: Image.asset(
-                    'images/onlynewsfoto1.jpeg',
-                    fit: BoxFit.cover, // Resmi tamamen kaplamak için
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        body: Column(
+      backgroundColor: HexColor(secondcolor),
+      appBar: AppBar(
+        toolbarHeight: 90,
+        backgroundColor: HexColor(primarycolor),
+        title: Row(
           children: [
-            //kategoriler
-            const Row(
-              children: [
-                Icon(
-                  Icons.category_outlined,
-                  size: 50,
+            SizedBox(
+              width: 0,
+            ),
+            ClipOval(
+              child: SizedBox(
+                width: 250,
+                height: 120,
+                child: Image.asset(
+                  'images/onlynewsfoto1.jpeg',
+                  fit: BoxFit.cover,
                 ),
-                Text(
-                  'Kategoriler',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-            //fotolu kategorileri ekliom
-            Row(
-              children: [
-                Expanded(child: cards(context, 'images/spor.jpeg', 'Spor')),
-                Expanded(child: cards(context, 'images/science.jpeg', 'Bilim')),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                    child: cards(context, 'images/eğitim.jpeg', 'Teknoloji')),
-                Expanded(
-                    child: cards(context, 'images/ekonomi.jpeg', 'Ekonomi')),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                    child: cards(
-                        context, 'images/rammstein-crowd.jpg', 'Eğlence')),
-                Expanded(child: cards(context, 'images/sağlık.jpeg', 'Sağlık')),
-              ],
+              ),
             ),
           ],
         ),
-        bottomNavigationBar: BottomAppBar(
-          color: HexColor(primarycolor),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      ),
+      body: Column(
+        children: [
+          const Row(
+            children: [
+              Icon(
+                Icons.category_outlined,
+                size: 50,
+              ),
+              Text(
+                'Kategoriler',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: cards(context, 'images/spor.jpeg', 'Spor', 'sport')),
+              Expanded(
+                  child: cards(
+                      context, 'images/science.jpeg', 'Bilim', 'science')),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: cards(context, 'images/eğitim.jpeg', 'Teknoloji',
+                      'technology')),
+              Expanded(
+                  child: cards(
+                      context, 'images/ekonomi.jpeg', 'Ekonomi', 'business')),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: cards(context, 'images/rammstein-crowd.jpg', 'Eğlence',
+                      'entertainment')),
+              Expanded(
+                  child:
+                      cards(context, 'images/sağlık.jpeg', 'Sağlık', 'health')),
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: HexColor(primarycolor),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
             IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.home,
-                  color: HexColor(secondcolor),
-                  size: 40,
-                )),
+              onPressed: () {},
+              icon: Icon(
+                Icons.home,
+                color: HexColor(secondcolor),
+                size: 40,
+              ),
+            ),
             IconButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ehoca()),
-                  );
-                },
-                icon: Icon(
-                  Icons.search,
-                  color: HexColor(secondcolor),
-                  size: 40,
-                )),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ehoca()),
+                );
+              },
+              icon: Icon(
+                Icons.search,
+                color: HexColor(secondcolor),
+                size: 40,
+              ),
+            ),
             IconButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => userinput()),
-                  );
-                },
-                icon: Icon(
-                  Icons.person,
-                  color: HexColor(secondcolor),
-                  size: 40,
-                )),
-          ]),
-        ));
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => userinput()),
+                );
+              },
+              icon: Icon(
+                Icons.person,
+                color: HexColor(secondcolor),
+                size: 40,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
